4d1d9f1aa990e3f3af17a9089edf4ad3fb74bb86
lucasmaochenyu <maochenyu.acc@gmail.com>
2022-07-06 16:53:43 -0700

    Prey

---
+++ Bird/src/Prey.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Prey extends BirdImp {
+
+  public Prey(String type, String definingCharacteristic, boolean extinction, int numberofwings,
+      ArrayList<Food> food) {
+    super(type, "sharp, hooks, beaks", extinction, 2, food);
+
+  }
+
+
+}
